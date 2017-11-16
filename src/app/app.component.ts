d9c7732bd4b274887d664c1b8e5451b6b15794e7
Juan Peist <juan@john.cat>
2017-11-16 11:27:10 +0100

    Add foundation styles [https://foundation.zurb.com/sites/docs/]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/css/vendor/foundation.css', './app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Bars';
 }
