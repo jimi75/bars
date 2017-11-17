b9e8fe19aa56cef3f530b563b51fed7bd33ec4a1
Juan Peist <juan@john.cat>
2017-11-17 08:38:20 +0100

    Add routes for bars listing and bar detail (not implemented yet)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule }     from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BarsComponent }        from './bars/bars.component';
+import { BarDetailComponent }   from './bar-detail/bar-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/bars', pathMatch: 'full' },
+  { path: 'bars/:id', component: BarDetailComponent },
+  { path: 'bars', component: BarsComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
