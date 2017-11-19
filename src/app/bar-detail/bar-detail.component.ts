32d486c3bddac7208c6871b32307b585e47c43dc
Juan Peist <juan@john.cat>
2017-11-19 21:17:21 +0100

    Better header and better code style in detail component

--- src/app/bar-detail/bar-detail.component.ts
+++ src/app/bar-detail/bar-detail.component.ts
@@ -9,20 +9,20 @@ import { BarService }     from '../bar.service';
 declare var L :any;
 
 @Component({
-  selector: 'app-bar-detail',
+  selector:    'app-bar-detail',
   templateUrl: './bar-detail.component.html',
-  styleUrls: ['./bar-detail.component.css']
+  styleUrls:   ['./bar-detail.component.css']
 })
 
 export class BarDetailComponent implements OnInit {
 
-  @Input() bar: Bar;
+  @Input()  bar: Bar;
   @Output() close = new EventEmitter();
 
-  map: any;
-  icon: any;
+  map:     any;
   address: string;
-
+  icon:    any = L.icon({ iconUrl: 'assets/marker.png', iconSize: [30, 56], iconAnchor: [15, 56] });
+  
   constructor(
     private route: ActivatedRoute,
     private location: Location,
@@ -40,34 +40,28 @@ export class BarDetailComponent implements OnInit {
   }
 
   makeBarMap(bar: Bar) {
-    
-    // TODO: Possible Make a leaflet service
+
+    // TODO: Make a leaflet service
     this.map = L.map('map', {
       center: [bar.lat, bar.long],
       zoom: 16,
       attributionControl: false
     });
 
-    this.icon = L.icon({
-      iconUrl: 'assets/marker.png',
-      iconSize: [30, 56],
-      iconAnchor: [15, 56]
-    });
-
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
     L.marker([ bar.lat, bar.long ], { icon: this.icon }).addTo(this.map);
     
-    // TOOD: Other service here
+    // TOOD: Other service here (reverse geocoder)
     this.http.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${bar.lat},${bar.long}&key=AIzaSyBPL8BMqAZWdh2Gxhnu29rjX6OrVBH4mt8`).subscribe(data => {
       this.address = data['results'][0]['formatted_address']; 
     });
   
-  };
+  }
 
-  onModalClosed() {
+  onModalClosed () {
     this.close.emit();
     this.router.navigate(['bars']);
-  };
+  }
 
   ngOnInit() {
     this.getBar();
